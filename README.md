6034b94259eeccd5dcc92698521d87f277f41ee4
PramodSri1709 <prasma403@gmail.com>
2021-07-27 15:41:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-favqh1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-favqh1)
---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import emojipedia from "../Emojipedia"
+import Card from "./Card"
+
+
+function entry(emojiTerm){
+
+  return(
+
+    <Card 
+    
+    key={emojiTerm.id}
+
+    emoji = {emojiTerm.emoji}
+
+    name = {emojiTerm.name}
+
+    meaning = {emojiTerm.meaning}
+
+    />
+
+  );
+
+}
+
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">
+      
+      {emojipedia.map(entry)}
+
+      </dl>
+    </div>
+  );
+}
+
+export default App;
